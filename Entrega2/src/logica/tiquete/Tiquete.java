aa531eeb97814eb6cb0135dfae1cf7d2e13e160e
sebascoco <s.salazars2@uniandes.edu.co>
2025-10-22 15:19:15 -0500

    Tiquete

--- Entrega2/src/logica/tiquete/Tiquete.java
+++ Entrega2/src/logica/tiquete/Tiquete.java
@@ -1,64 +1,113 @@
 package logica.tiquete;
 
 import logica.evento.Localidad;
+import logica.evento.Evento;
+import logica.usuario.Cliente;
 
 public abstract class Tiquete {
-	protected String identificador;
-	protected double precioBase;
+
+    protected String identificador;
+    protected double precioBase;
     protected double cargoPorcentual;
     protected double cuotaAdicionalEmision;
     protected Localidad localidad;
+    protected Evento evento;
+    protected Cliente dueno;
     protected String fecha;
     protected String hora;
     protected String estado;
-	
+
     public Tiquete(String identificador, double precioBase, double cargoPorcentual,
-        double cuotaAdicionalEmision, Localidad localidad, String fecha, String hora, String estado) {
-    	this.identificador = identificador;
-    	this.precioBase = precioBase;
-    	this.cargoPorcentual = cargoPorcentual;
-    	this.cuotaAdicionalEmision = cuotaAdicionalEmision;
-    	this.localidad = localidad;
-    	this.fecha = fecha;
-    	this.hora = hora;
-    	this.estado = estado;
+                   double cuotaAdicionalEmision, Localidad localidad, Evento evento,
+                   String fecha, String hora, String estado) {
+
+        this.identificador = identificador;
+        this.precioBase = precioBase;
+        this.cargoPorcentual = cargoPorcentual;
+        this.cuotaAdicionalEmision = cuotaAdicionalEmision;
+        this.localidad = localidad;
+        this.evento = evento;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estado = estado;
+        this.dueno = null;
     }
 
-	public String getIdentificador() {
-		return identificador;
-	}
+    public String getIdentificador() {
+        return identificador;
+    }
 
-	public double getPrecioBase() {
-		return precioBase;
-	}
+    public double getPrecioBase() {
+        return precioBase;
+    }
 
-	public double getCargoPorcentual() {
-		return cargoPorcentual;
-	}
+    public double getCargoPorcentual() {
+        return cargoPorcentual;
+    }
 
-	public double getCuotaAdicionalEmision() {
-		return cuotaAdicionalEmision;
-	}
+    public double getCuotaAdicionalEmision() {
+        return cuotaAdicionalEmision;
+    }
 
-	public Localidad getLocalidad() {
-		return localidad;
-	}
+    public Localidad getLocalidad() {
+        return localidad;
+    }
 
-	public String getFecha() {
-		return fecha;
-	}
+    public Evento getEvento() {
+        return evento;
+    }
 
-	public String getHora() {
-		return hora;
-	}
+    public Cliente getDueno() {
+        return dueno;
+    }
 
-	public String getEstado() {
-		return estado;
-	}
+    public void setDueno(Cliente dueno) {
+        this.dueno = dueno;
+    }
 
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-	
-	public abstract double calcularPrecio();
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public boolean isVendido() {
+        return "vendido".equalsIgnoreCase(estado);
+    }
+
+    public void marcarComoVendido(Cliente comprador) {
+        this.dueno = comprador;
+        this.estado = "vendido";
+    }
+
+    public void marcarComoReembolsado() {
+        this.estado = "reembolsado";
+    }
+
+    public void marcarComoTransferido(Cliente nuevoDueno) {
+        this.dueno = nuevoDueno;
+        this.estado = "transferido";
+    }
+
+    public abstract double calcularPrecio();
+
+    @Override
+    public String toString() {
+        return "Tiquete{" +
+                "id='" + identificador + '\'' +
+                ", localidad=" + (localidad != null ? localidad.getNombre() : "sin localidad") +
+                ", evento=" + (evento != null ? evento.getNombre() : "sin evento") +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
 }
